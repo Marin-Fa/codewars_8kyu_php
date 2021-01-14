09d31930c03c0cdc56af5743f8f806d5d1e91b75
Marin-Fa <marin.fayolle@gmail.com>
2021-01-14 09:56:15 +0100

    Make a function that receive age, and return what they drink.

---
+++ Drink about.php
@@ -0,0 +1,12 @@
+<?php
+function people_with_age_drink(int $old): string {
+    if($old<14) {
+        return "drink toddy";
+    } else if($old<18) {
+        return "drink coke";
+    } else if($old<21) {
+        return "drink beer";
+    } else if($old>=21) {
+        return "drink whisky";
+    }
+}
