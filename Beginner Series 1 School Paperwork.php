d776eeac4f39a781425ad01d9943256c3272e3e0
Marin-Fa <marin.fayolle@gmail.com>
2021-01-18 10:14:09 +0100

    Your task is to calculate how many blank pages do you need.

---
+++ Beginner Series 1 School Paperwork.php
@@ -0,0 +1,10 @@
+<?php
+function paperwork(int $n, int $m): int
+{
+//   if ($n < 0 || $m < 0) {
+//      return 0;
+//    } else {
+//      return $n * $m;
+//    }
+    return $n > 0 && $m > 0 ? $n * $m : 0;
+}
